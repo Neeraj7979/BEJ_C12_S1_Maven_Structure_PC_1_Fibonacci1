46a350e1717906c03e5561fa26e489a8e57908c4
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 16:17:38 +0530

    Final commit

--- src/main/java/com/niit/bej/Main.java
+++ src/main/java/com/niit/bej/Main.java
@@ -2,6 +2,8 @@ package com.niit.bej;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       FibonacciSeries fibonacciSeries =new FibonacciSeries();
+       fibonacciSeries.printFibonacciSeries(5);
+
     }
 }
