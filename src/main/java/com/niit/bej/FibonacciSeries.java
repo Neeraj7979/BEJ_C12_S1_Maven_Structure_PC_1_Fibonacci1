831c1c7f58917c693992b452e56941688ec99db3
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 16:16:46 +0530

    Create method to print fibonacci series

--- src/main/java/com/niit/bej/FibonacciSeries.java
+++ src/main/java/com/niit/bej/FibonacciSeries.java
@@ -1,4 +1,16 @@
 package com.niit.bej;
 
 public class FibonacciSeries {
+    public void printFibonacciSeries(int numberOfTerms) {
+        int firstTerm = 0;
+        int secondTerm = 1;
+        int nextTerm = 0;
+        System.out.print(firstTerm + "" + secondTerm);
+        for (int i = 2; i < numberOfTerms; ++i) {
+            nextTerm = firstTerm + secondTerm;
+            System.out.print(nextTerm);
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
 }
